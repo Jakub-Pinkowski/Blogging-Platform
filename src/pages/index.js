4d6b73ea052e75cf8ca587a2ee6a68f3138d854a
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-17 22:34:31 +0200

    Added a toast

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
+import toast from 'react-hot-toast'
 
 import Loader from '@/components/Loader'
 
 export default function Home() {
     return (
-        <main>
-            <div>
-                <Loader show />
-                <h1> Hello Index Page</h1>
-            </div>
-        </main>
+        <div>
+            <Loader show />
+            <h1> Hello Index Page</h1>
+            <button onClick={() => toast.success('hello toast')}>Toast Me</button>
+        </div>
     )
 }
