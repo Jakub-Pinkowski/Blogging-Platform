d3aa6db2a324e45d82e4c8c935ed4f5243fca6cf
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-17 22:57:19 +0200

    Added new imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import '@/styles/globals.css'
-import Navbar from '@/components/Navbar'
+
 import { Toaster } from 'react-hot-toast'
+import { useState, useEffect } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+
+import { UserContext } from '@/lib/context'
+import Navbar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }) {
     return (
-        <>
+        <UserContext.Provider value={{ user: {}, username: 'johndoe' }}>
             <Navbar />
             <Component {...pageProps} />
             <Toaster />
-        </>
+        </UserContext.Provider>
     )
 }
