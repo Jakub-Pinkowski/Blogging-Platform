01ba570ec32adc0ee37c55584c3fff45d2c0fadc
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-17 22:29:00 +0200

    Created navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+import Loader from '@/components/Loader'
 
 export default function Home() {
     return (
         <main>
             <div>
+                <Loader show />
                 <h1> Hello Index Page</h1>
             </div>
         </main>
