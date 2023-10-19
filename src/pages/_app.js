020e5c5f6b9c037ba912418523ddcb01e7429f88
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-19 22:27:05 +0200

    Fixed styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,12 @@ import '@/styles/globals.css'
 import { Toaster } from 'react-hot-toast'
 import { useState, useEffect } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
-
 import { UserContext } from '@/lib/context'
+
 import Navbar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }) {
-    
+
 
     return (
         <UserContext.Provider value={{ user: {}, username: 'johndoe' }}>
