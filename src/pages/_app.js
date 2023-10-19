db32d2b10bd44af4fb2583cde0aca03cef28a5e9
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-19 22:55:55 +0200

    Finished logging in module

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,16 @@
 import '@/styles/globals.css'
 
 import { Toaster } from 'react-hot-toast'
-import { useState, useEffect } from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { UserContext } from '@/lib/context'
 
 import Navbar from '@/components/Navbar'
+import { UserContext } from '@/lib/context'
+import { useUserData } from '../lib/hooks'
 
 export default function App({ Component, pageProps }) {
-
+    const userData = useUserData()
 
     return (
-        <UserContext.Provider value={{ user: {}, username: 'johndoe' }}>
+        <UserContext.Provider value={userData}>
             <Navbar />
             <Component {...pageProps} />
             <Toaster />
