6f2d0805da595d78688b70e1b2eaa0ede30bd7f4
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-19 23:32:44 +0200

    Added new components

--- src/pages/[username]/index.js
+++ src/pages/[username]/index.js
@@ -1,5 +1,11 @@
+import UserProfile from '../../components/UserProfile'
+import PostFeed from '../../components/PostFeed'
+
 export default function UserPage({}) {
-    return <main>
-        <h1>User Page</h1>
-    </main>
+    return (
+        <main>
+            <UserProfile user={user} />
+            <PostFeed posts={posts} />
+        </main>
+    )
 }
