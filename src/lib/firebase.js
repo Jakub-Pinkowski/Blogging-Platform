7a9a2cb718a1265f8d276a41ade3df7564100b07
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-20 21:29:12 +0200

    Updated lib

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -16,22 +16,26 @@ if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig)
 }
 
+// Auth exports
 export const auth = firebase.auth()
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
+// Firestore exports
 export const firestore = firebase.firestore()
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp
+export const fromMillis = firebase.firestore.Timestamp.fromMillis
+export const increment = firebase.firestore.FieldValue.increment
+
+// Storage exports
 export const storage = firebase.storage()
 export const STATE_CHANGED = firebase.storage.TaskEvent.STATE_CHANGED
-export const fromMillis = firebase.firestore.Timestamp.fromMillis
-export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp
 
-// Helper functions
+/// Helper functions
 
-/**
+/**`
  * Gets a users/{uid} document with username
  * @param  {string} username
  */
-
 export async function getUserWithUsername(username) {
     const usersRef = firestore.collection('users')
     const query = usersRef.where('username', '==', username).limit(1)
@@ -39,17 +43,16 @@ export async function getUserWithUsername(username) {
     return userDoc
 }
 
-/**
+/**`
  * Converts a firestore document to JSON
  * @param  {DocumentSnapshot} doc
  */
-
 export function postToJSON(doc) {
     const data = doc.data()
     return {
         ...data,
         // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
-        createdAt: data.createdAt.toMillis(),
-        updatedAt: data.updatedAt.toMillis(),
+        createdAt: data?.createdAt.toMillis() || 0,
+        updatedAt: data?.updatedAt.toMillis() || 0,
     }
 }
