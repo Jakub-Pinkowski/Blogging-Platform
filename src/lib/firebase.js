0465cdc003cd88424494333ad952f6cd77e76dc1
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-20 19:27:31 +0200

    Added Admin page

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -18,8 +18,11 @@ if (!firebase.apps.length) {
 
 export const auth = firebase.auth()
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+
 export const firestore = firebase.firestore()
 export const storage = firebase.storage()
+export const fromMillis = firebase.firestore.Timestamp.fromMillis
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp
 
 // Helper functions
 
@@ -49,5 +52,3 @@ export function postToJSON(doc) {
         updatedAt: data.updatedAt.toMillis(),
     }
 }
-
-export const fromMillis = firebase.firestore.Timestamp.fromMillis
